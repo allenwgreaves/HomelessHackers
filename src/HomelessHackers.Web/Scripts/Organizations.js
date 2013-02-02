569dfc2c8af236f7f98bf4f019a6a2db4c650e73
Adam <adam@adammartinek.com>
2013-02-02 11:13:38 -0800

    moved default.html js to own file.

---
+++ src/HomelessHackers.Web/Scripts/Organizations.js
@@ -0,0 +1,33 @@
+﻿
+        function OrganizationViewModel()
+        {
+            var self = this;
+            self.Organizations = ko.observableArray([]);
+            self.NewOrganization = ko.observable();
+            self.GetOrganizations = function () {
+                $.getJSON("/api/organizations/", null, function (data){
+                    self.Organizations(data);
+                });
+            };
+            self.SaveOrganization = function () {
+                $.ajax({
+                    type: "POST",
+                    url: "/api/organizations/",
+                    data: self.NewOrganization(),
+                    dataType: "Organization",
+                    success: function (msg) {
+                        alert("I did it: " + msg);
+                    }
+                });
+            };
+        }
+$(document).ready(function () {
+
+    var viewmodel = new OrganizationViewModel();
+
+    viewmodel.GetOrganizations();
+
+    ko.applyBindings(viewmodel);
+                
+});
+
