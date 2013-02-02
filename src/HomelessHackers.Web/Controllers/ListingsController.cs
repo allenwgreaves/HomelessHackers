13de3ef902f3318a65277c0a9e90cd8c3df4994b
adamskt <kelly@intellitect.com>
2013-02-01 22:50:24 -0800

    Adding scaffold controllers for Listings and Ogranizations

---
+++ src/HomelessHackers.Web/Controllers/ListingsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HomelessHackers.Web.Controllers
+{
+    public class ListingsController : ApiController
+    {
+        // GET api/listings
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/listings/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/listings
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/listings/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/listings/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
